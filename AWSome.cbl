3fb8b67a22177a69fe5f3f314bdefa8e28c5220c
Mehdi Salehi <mehdi@awsengineer.com>
2021-01-14 23:39:39 +1100

    AWSome cobol code

---
+++ AWSome.cbl
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  hello.
+
+       PROCEDURE DIVISION.
+       DisplayPrompt.
+           DISPLAY "G'day! I am a COBOL code running on AIX :)".
+           STOP RUN.
