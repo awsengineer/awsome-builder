a731dc8835d8b529940488a6a23cf431f220f93b
Mehdi Salehi <mehdi@awsengineer.com>
2021-01-29 22:42:16 +1100

    ABIII edit 1

--- AWSome.cbl
+++ AWSome.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID.  AWSome.
        PROCEDURE DIVISION.
        DisplayPrompt.
-           DISPLAY "G'day! I am a COBOL code running on AIX. Have a great day. :)".
+           DISPLAY "G'day! I am a COBOL code running on AIX. Have a great day :)".
            STOP RUN.
