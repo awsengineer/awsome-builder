02a1d2f3b02f35b5263784e52841a0baf54c4e2f
Mehdi Salehi <mehdi@awsengineer.com>
2021-01-29 23:12:37 +1100

    ABIII edit 100

--- AWSome.cbl
+++ AWSome.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID.  ABIII.
        PROCEDURE DIVISION.
        DisplayPrompt.
-           DISPLAY "Folks! I am a COBOL code running on AIX.".
+           DISPLAY "Hello team! I am a COBOL code running on AIX.".
            STOP RUN.
