94ac2fce0eda349258711ab834eb9075f515a39c
Mehdi Salehi <mehdi@awsengineer.com>
2021-01-31 13:28:46 +1100

    ABIII change

--- AWSome.cbl
+++ AWSome.cbl
@@ -1,7 +1,6 @@
-    IDENTIFICATION DIVISION.
-    PROGRAM-ID.  ABIII.
-
-    PROCEDURE DIVISION.
-    DisplayPrompt.
-        DISPLAY "OK! Let's automate the stuffs!".
-        STOP RUN.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  ABIII.
+       PROCEDURE DIVISION.
+       DisplayPrompt.
+           DISPLAY "Good day! I am a freaking code running on AIX.".
+           STOP RUN.
