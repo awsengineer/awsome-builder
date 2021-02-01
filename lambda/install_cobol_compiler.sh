11388955b44c29f21a9750edd0a07e8a5b67c67b
Mehdi Salehi <mehdi@awsengineer.com>
2021-02-01 18:20:20 +1100

    ABIII change

--- lambda/install_cobol_compiler.sh
+++ lambda/install_cobol_compiler.sh
@@ -1,6 +1,6 @@
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum --enablerepo=epel install -y wget gawk make gcc automake byacc gmp mpir-devel libdb-devel xz >/dev/null 2>&1
-wget https://udomain.dl.sourceforge.net/project/gnucobol/gnucobol/3.1/gnucobol-3.1.2.tar.xz >/dev/null >/dev/null 2>&1
+wget https://raw.githubusercontent.com/awsengineer/awsome-builder/main/gnucobol-3.1.2.tar.xz >/dev/null >/dev/null 2>&1
 xz -d gnucobol-3.1.2.tar.xz
 tar xvf gnucobol-3.1.2.tar >/dev/null 2>&1
 cd gnucobol-3.1.2
