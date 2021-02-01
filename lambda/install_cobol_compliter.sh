55e4a1f3c8de7da2e2bfb6ad6e5208a61a58be14
Mehdi Salehi <mehdi@awsengineer.com>
2021-02-01 17:26:10 +1100

    ABIII change

--- lambda/install_cobol_compliter.sh
+++ lambda/install_cobol_compliter.sh
@@ -1,4 +1,4 @@
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum --enablerepo=epel install -y wget gawk make gcc automake byacc gmp mpir-devel libdb-devel
 wget https://udomain.dl.sourceforge.net/project/gnucobol/gnucobol/3.1/gnucobol-3.1.2.tar.xz >/dev/null >/dev/null 2>&1
 tar xvf gnucobol-3.1.2.tar.xz >/dev/null 2>&1
