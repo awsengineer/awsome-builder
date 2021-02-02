49d8b8b4e323394d6db00076cb66b75e750d3dc1
Mehdi Salehi <mehdi@awsengineer.com>
2021-02-02 16:38:50 +1100

    ABIII change

---
+++ clean_images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function remove_docker_image {
+        set -x
+        docker_image=$1
+        docker images | grep $docker_image
+        if [ $? -eq 0 ]
+        then
+                docker ps -a | grep $docker_image
+                if [ $? -eq 0 ]
+                then
+                        docker ps -a | grep $docker_image | \
+                        while read container_id junk ; do docker container rm -f $container_id; done >/dev/null 2>&1
+                fi
+                docker image rm -f ezzobad/awsome-builder:$docker_image
+        else
+                echo "The image ($docker_image) dose not exist."
+        fi
+}
+remove_docker_image "cobol-base"
+remove_docker_image "cobol-lambda"
+remove_docker_image "lambda-test"
