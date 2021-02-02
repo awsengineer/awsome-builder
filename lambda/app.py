5a547b57b655debee29e05969e963978228cc939
Mehdi Salehi <mehdi@awsengineer.com>
2021-02-02 12:53:22 +1100

    ABIII change

--- lambda/app.py
+++ lambda/app.py
@@ -2,4 +2,4 @@ import sys, subprocess
 def handler(event, context):
     cobol_app_result="/usr/local/bin/cobol_app"
     print (subprocess.check_output(cobol_app_result, shell=True))
-    return
+    return ("Nice! You COBOL-based Lambda function is working :)")
